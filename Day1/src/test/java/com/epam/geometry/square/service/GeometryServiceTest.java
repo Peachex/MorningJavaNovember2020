17446786d70f069cf349634bc7cb199acc12e8e6
Peachex <klevolex@gmail.com>
2020-11-16 21:30:19 +0300

    fix test

--- Day1/src/test/java/com/epam/geometry/square/service/GeometryServiceTest.java
+++ Day1/src/test/java/com/epam/geometry/square/service/GeometryServiceTest.java
@@ -6,46 +6,56 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
 
 public class GeometryServiceTest {
-    GeometryService service = new GeometryService();
-
     @Test
     public void findAreaTest() {
-        double expected = 4;
         double sideLength = 2;
-        double actual = service.findArea(sideLength);
+        Square square = new Square(sideLength);
+        double expected = Math.pow(square.getSideLength(), 2);
+        GeometryService service = new GeometryService();
+        double actual = service.findArea(square);
         assertEquals(actual, expected);
     }
 
     @Test
     public void findSquareLengthByAreaTest() {
-        double expected = 2;
         double area = 4;
+        double expected = Math.sqrt(area);
+        GeometryService service = new GeometryService();
         double actual = service.findSquareLengthByArea(area);
         assertEquals(actual, expected);
     }
 
     @Test
     public void findSquareLengthByRadiusTest() {
-        double expected = 2;
-        Circle circle = new Circle(Math.sqrt(2));
+        double radius = Math.sqrt(2);
+        Circle circle = new Circle(radius);
+        double expected = circle.getRadius() * 2 / Math.sqrt(2);
+        GeometryService service = new GeometryService();
         double actual = service.findSquareLengthByRadius(circle);
         assertEquals(actual, expected);
     }
 
     @Test
     public void findRadiusBySquareLengthTest() {
-        double expected = 2;
-        Square square = new Square(4, 16);
+        double sideLength = 4;
+        Square square = new Square(sideLength);
+        double expected = sideLength / 2;
+        GeometryService service = new GeometryService();
         double actual = service.findRadiusBySquareLength(square);
         assertEquals(actual, expected);
     }
 
     @Test
     public void findAreaRatioTest() {
-        double expected = 4;
-        Square square1 = new Square(4, 16);
-        Square square2 = new Square(2, 4);
-        double actual = service.findAreaRatio(square1, square2);
+        double sideLength1 = 4;
+        Square square1 = new Square(sideLength1);
+        double sideLength2 = 2;
+        Square square2 = new Square(sideLength2);
+        double area1 = 16;
+        double area2 = 4;
+        double expected = area1 / area2;
+        GeometryService service = new GeometryService();
+        double actual = service.findAreaRatio(area1, area2);
         assertEquals(actual, expected);
     }
 }
