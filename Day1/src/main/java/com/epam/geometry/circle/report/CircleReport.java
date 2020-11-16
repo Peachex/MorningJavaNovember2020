66b70d4c3421cf68ceb512b471aaadc3fe600343
Peachex <klevolex@gmail.com>
2020-11-16 21:02:07 +0300

    fix report function

--- Day1/src/main/java/com/epam/geometry/circle/report/CircleReport.java
+++ Day1/src/main/java/com/epam/geometry/circle/report/CircleReport.java
@@ -3,7 +3,7 @@ package com.epam.geometry.circle.report;
 import com.epam.geometry.circle.entity.Circle;
 
 public class CircleReport {
-    public void reportCircle(Circle circle) {
-        System.out.println("R = " + circle.getRadius() + "\nL = " + circle.getCircleLength() + "\nS = " + circle.getArea());
+    public void reportCircle(Circle circle, double circleLength, double circleArea) {
+        System.out.println("R = " + circle.getRadius() + "\nL = " + circleLength + "\nS = " + circleArea);
     }
 }
