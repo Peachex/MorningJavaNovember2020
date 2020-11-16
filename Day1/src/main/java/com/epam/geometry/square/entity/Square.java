b90397e498b40390784603f93759c5a98ca38512
Peachex <klevolex@gmail.com>
2020-11-16 21:28:55 +0300

    remove variable

--- Day1/src/main/java/com/epam/geometry/square/entity/Square.java
+++ Day1/src/main/java/com/epam/geometry/square/entity/Square.java
@@ -2,11 +2,9 @@ package com.epam.geometry.square.entity;
 
 public class Square {
     private double sideLength;
-    private double area;
 
-    public Square(double sideLength, double area) {
+    public Square(double sideLength) {
         this.sideLength = sideLength;
-        this.area = area;
     }
 
     public double getSideLength() {
@@ -17,19 +15,10 @@ public class Square {
         this.sideLength = sideLength;
     }
 
-    public double getArea() {
-        return this.area;
-    }
-
-    public void setArea(double area) {
-        this.area = area;
-    }
-
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("Square{");
         sb.append("sideLength=").append(sideLength);
-        sb.append(", area=").append(area);
         sb.append('}');
         return sb.toString();
     }
