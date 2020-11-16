2f1d5c0f0ef3393201a1dc83db21ed1c39a4e7ec
Peachex <klevolex@gmail.com>
2020-11-16 21:05:50 +0300

    fix function

--- Day1/src/main/java/com/epam/geometry/circle/service/CircleService.java
+++ Day1/src/main/java/com/epam/geometry/circle/service/CircleService.java
@@ -3,13 +3,13 @@ package com.epam.geometry.circle.service;
 import com.epam.geometry.circle.entity.Circle;
 
 public class CircleService {
-    public double findCircleArea(double radius) {
-        double result = Math.PI * Math.pow(radius, 2);
+    public double findCircleArea(Circle circle) {
+        double result = Math.PI * Math.pow(circle.getRadius(), 2);
         return result;
     }
 
-    public double findCircleLength(double radius) {
-        double result = 2 * Math.PI * radius;
+    public double findCircleLength(Circle circle) {
+        double result = 2 * Math.PI * circle.getRadius();
         return result;
     }
 }
