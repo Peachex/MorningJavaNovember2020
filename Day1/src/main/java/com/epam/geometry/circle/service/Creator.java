78eac64d1685e1cc4c73df5c7c6588c86329fe80
Peachex <klevolex@gmail.com>
2020-11-16 20:55:26 +0300

    remove creator

--- Day1/src/main/java/com/epam/geometry/circle/service/Creator.java
+++
@@ -1,9 +0,0 @@
-package com.epam.geometry.circle.service;
-
-import com.epam.geometry.circle.entity.*;
-
-public class Creator {
-    public Circle createCircle(double radius, CircleService service) {
-        return new Circle(radius, service.findCircleLength(radius), service.findCircleArea(radius));
-    }
-}
