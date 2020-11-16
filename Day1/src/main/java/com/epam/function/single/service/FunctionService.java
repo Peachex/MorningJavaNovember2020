ae5b33dfe0b287af6dd17e8bb649e0c84adbd32b
Peachex <klevolex@gmail.com>
2020-11-16 20:40:03 +0300

    fix function

--- Day1/src/main/java/com/epam/function/single/service/FunctionService.java
+++ Day1/src/main/java/com/epam/function/single/service/FunctionService.java
@@ -3,10 +3,10 @@ package com.epam.function.single.service;
 import com.epam.function.single.entity.Function;
 
 public class FunctionService {
-    public Function findFunctionByArg(double arg) {
-        double functionValue = 0;
+    public double findFunctionByArg(double arg) {
+        double result = 0;
         if (arg >= 3) {
-            functionValue = -Math.pow(arg, 2) + 3 * arg + 9;
+            result = -Math.pow(arg, 2) + 3 * arg + 9;
         } else {
             try {
                 arg = 1 / (Math.pow(arg, 3) - 6);
@@ -14,6 +14,6 @@ public class FunctionService {
                 System.out.println(e);
             }
         }
-        return new Function(functionValue, arg);
+        return result;
     }
 }
