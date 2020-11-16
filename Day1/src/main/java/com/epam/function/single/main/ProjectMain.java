b09e81d83822bdefca7d7694f5ced85ea5c6faf6
Peachex <klevolex@gmail.com>
2020-11-16 20:50:31 +0300

    fix function main

--- Day1/src/main/java/com/epam/function/single/main/ProjectMain.java
+++ Day1/src/main/java/com/epam/function/single/main/ProjectMain.java
@@ -1,15 +1,16 @@
 package com.epam.function.single.main;
 
-import com.epam.function.single.entity.Function;
+import com.epam.function.single.entity.*;
 import com.epam.function.single.report.FunctionReport;
 import com.epam.function.single.service.FunctionService;
 
 public class ProjectMain {
     public static void main(String[] args) {
+        int argumentValue = 3;
+        Argument argument = new Argument(argumentValue);
         FunctionService service = new FunctionService();
-        int argument = 3;
-        Function function = service.findFunctionByArg(argument);
+        Function function = new Function(service.findFunctionByArg(argument.getValue()));
         FunctionReport reporter = new FunctionReport();
-        reporter.reportFunction(function);
+        reporter.reportFunction(function, argument);
     }
 }
