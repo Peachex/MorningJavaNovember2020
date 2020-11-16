c0439bc46112f7a9fcd0069197dfaff67e5a88d0
Peachex <klevolex@gmail.com>
2020-11-16 20:34:36 +0300

    remove variable

--- Day1/src/main/java/com/epam/function/single/entity/Function.java
+++ Day1/src/main/java/com/epam/function/single/entity/Function.java
@@ -2,11 +2,9 @@ package com.epam.function.single.entity;
 
 public class Function {
     private double value;
-    private double arg;
 
-    public Function(double value, double arg) {
+    public Function(double value) {
         this.value = value;
-        this.arg = arg;
     }
 
     public double getValue() {
@@ -17,19 +15,10 @@ public class Function {
         this.value = value;
     }
 
-    public double getArg() {
-        return this.arg;
-    }
-
-    public void setArg(double arg) {
-        this.arg = arg;
-    }
-
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("Function{");
         sb.append("value=").append(value);
-        sb.append(", arg=").append(arg);
         sb.append('}');
         return sb.toString();
     }
