fb426d8782b5dae978b94d42b8e5fc0db85b0bb3
Peachex <klevolex@gmail.com>
2020-11-16 20:40:55 +0300

    fix report function

--- Day1/src/main/java/com/epam/function/single/report/FunctionReport.java
+++ Day1/src/main/java/com/epam/function/single/report/FunctionReport.java
@@ -1,9 +1,9 @@
 package com.epam.function.single.report;
 
-import com.epam.function.single.entity.Function;
+import com.epam.function.single.entity.*;
 
 public class FunctionReport {
-    public void reportFunction(Function function) {
-        System.out.println("F(x) = " + function.getValue() + ", x = " + function.getArg());
+    public void reportFunction(Function function, Argument argument) {
+        System.out.println("F(x) = " + function.getValue() + ", x = " + argument.getValue());
     }
 }
