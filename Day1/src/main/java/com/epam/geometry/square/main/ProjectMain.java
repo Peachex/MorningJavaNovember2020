ecd1f4a81bcba45fb2aea52ce8ea4ca6e4202348
Peachex <klevolex@gmail.com>
2020-11-16 21:29:24 +0300

    fix main

--- Day1/src/main/java/com/epam/geometry/square/main/ProjectMain.java
+++ Day1/src/main/java/com/epam/geometry/square/main/ProjectMain.java
@@ -3,18 +3,19 @@ package com.epam.geometry.square.main;
 import com.epam.geometry.square.entity.Circle;
 import com.epam.geometry.square.entity.Square;
 import com.epam.geometry.square.report.GeometricReport;
-import com.epam.geometry.square.service.Creator;
 import com.epam.geometry.square.service.GeometryService;
 
 public class ProjectMain {
     public static void main(String[] args) {
-        Creator creator = new Creator();
+        double sideLength = 2;
+        Square square = new Square(sideLength);
         GeometryService service = new GeometryService();
-        Square square = creator.createSquare(2, service);
-        Circle inscribedCircle = creator.createInscribedCircle(square, service);
-        Square inscribedSquare = creator.createInscribedSquare(inscribedCircle, service);
-        double areaRatio = service.findAreaRatio(square, inscribedSquare);
+        Circle inscribedCircle = new Circle(service.findRadiusBySquareLength(square));
+        Square inscribedSquare = new Square(service.findSquareLengthByRadius(inscribedCircle));
+        double squareArea = service.findArea(square);
+        double inscribedSquareArea = service.findArea(inscribedSquare);
+        double areaRatio = service.findAreaRatio(squareArea, inscribedSquareArea);
         GeometricReport reporter = new GeometricReport();
-        reporter.reportSquareArea(square, inscribedSquare, areaRatio);
+        reporter.reportSquareArea(squareArea, inscribedSquareArea, areaRatio);
     }
 }
