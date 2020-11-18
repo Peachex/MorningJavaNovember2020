d672d524a056263bf40e081c295bccaa40219d81
Peachex <klevolex@gmail.com>
2020-11-18 21:13:18 +0300

    change method parameters

--- Day1/src/main/java/com/epam/calendar/report/CalendarReport.java
+++ Day1/src/main/java/com/epam/calendar/report/CalendarReport.java
@@ -2,10 +2,9 @@ package com.epam.calendar.report;
 
 import com.epam.calendar.entity.Calendar;
 
-import java.time.LocalDate;
-
 public class CalendarReport {
-    public void reportCalendar(Calendar calendar, int daysAmount) {
-        System.out.println(calendar.getDate() + " - " + daysAmount + " day(s).");
+    public void reportCalendar(Calendar calendar, int daysAmount, boolean hasExtraDay) {
+        System.out.println(calendar.getDate() + " - " + daysAmount + " day(s).\n" + (hasExtraDay? "This year has got " +
+                "extra day." : "This year hasn't got extra day."));
     }
 }
