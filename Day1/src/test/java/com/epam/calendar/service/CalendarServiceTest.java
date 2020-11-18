d5b67782536ca218689233a1feba94ac380b360a
Peachex <klevolex@gmail.com>
2020-11-18 21:11:22 +0300

    add test

--- Day1/src/test/java/com/epam/calendar/service/CalendarServiceTest.java
+++ Day1/src/test/java/com/epam/calendar/service/CalendarServiceTest.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 import java.time.LocalDate;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class CalendarServiceTest {
     @Test
@@ -13,7 +14,15 @@ public class CalendarServiceTest {
         Calendar calendar = new Calendar(LocalDate.of(2021, 2, 2));
         CalendarService service = new CalendarService();
         int expected = 28;
-        int actual = service.findDaysAmount(calendar.getDate());
+        int actual = service.findDaysAmount(calendar);
         assertEquals(actual, expected);
+    }
+
+    @Test
+    public void checkYearForExtraDayTest() {
+        Calendar calendar = new Calendar(LocalDate.of(2020, 2, 2));
+        CalendarService service = new CalendarService();
+        boolean condition = service.checkYearForExtraDay(calendar);
+        assertTrue(condition);
     }
 }
