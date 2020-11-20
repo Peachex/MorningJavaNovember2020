d35c08eefa2ef5e63477b1d42c3f6d60b00f14bf
Peachex <klevolex@gmail.com>
2020-11-20 15:14:30 +0300

    change function parameter

--- Day3/src/main/java/com/epam/airline/report/AirlineReport.java
+++ Day3/src/main/java/com/epam/airline/report/AirlineReport.java
@@ -1,11 +1,15 @@
 package com.epam.airline.report;
 
-import com.epam.airline.entity.*;
+import com.epam.airline.entity.Airline;
 
 import java.util.Set;
 
 public class AirlineReport {
-    public void reportAirline(Set<Airline> airlines) {
-        System.out.println(airlines + "\n");
+    public void reportAirline(Set<Airline> airlines, StringBuilder message) {
+        System.out.println(message + ":\n");
+        for (Airline airline : airlines) {
+            System.out.println(airline);
+        }
+        System.out.println("------------------------------");
     }
 }
