a4196711392dea30eb2ad433cdc174916b32b6aa
Peachex <klevolex@gmail.com>
2020-11-20 15:12:57 +0300

    add new variable

--- Day3/src/main/java/com/epam/airline/main/ProjectMain.java
+++ Day3/src/main/java/com/epam/airline/main/ProjectMain.java
@@ -1,6 +1,7 @@
 package com.epam.airline.main;
 
-import com.epam.airline.entity.*;
+import com.epam.airline.entity.Airline;
+import com.epam.airline.entity.Airport;
 import com.epam.airline.report.AirlineReport;
 import com.epam.airline.service.AirportService;
 
@@ -18,6 +19,21 @@ public class ProjectMain {
         DayOfWeek dayOfWeek = DayOfWeek.MONDAY;
         boolean fromMinToMax = true;
 
+        StringBuilder message1 = new StringBuilder();
+        message1.append("Flights to ").append(destination);
+
+        StringBuilder message2 = new StringBuilder();
+        message2.append("Flights on ").append(dayOfWeek);
+
+        StringBuilder message3 = new StringBuilder();
+        message3.append("Flights on ").append(dayOfWeek).append(", after ").append(time);
+
+        StringBuilder message4 = new StringBuilder();
+        message4.append("Flights on ").append(planeType);
+
+        StringBuilder message5 = new StringBuilder();
+        message5.append("Flights, sorted by day of week");
+
         Set<Airline> result1 = service.findFlightByDestination(destination, airport);
         Set<Airline> result2 = service.findFlightByDayOfWeek(dayOfWeek, airport);
         Set<Airline> result3 = service.findFlightByDayOfWeekAndTime(dayOfWeek, time, airport);
@@ -25,10 +41,10 @@ public class ProjectMain {
         Set<Airline> result5 = service.sortFlightByDayOfWeek(fromMinToMax, airport);
 
         AirlineReport reporter = new AirlineReport();
-        reporter.reportAirline(result1);
-        reporter.reportAirline(result2);
-        reporter.reportAirline(result3);
-        reporter.reportAirline(result4);
-        reporter.reportAirline(result5);
+        reporter.reportAirline(result1, message1);
+        reporter.reportAirline(result2, message2);
+        reporter.reportAirline(result3, message3);
+        reporter.reportAirline(result4, message4);
+        reporter.reportAirline(result5, message5);
     }
 }
